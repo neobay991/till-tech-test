8b319fc0ceb28baf40b941f47e4a6c9601caebde
Jay Khan <jkhan992@gmail.com>
2018-08-16 11:43:14 +0100

    TESTS Change format of output of transaction

--- jasmine/jasmine-standalone-3.1.0/spec/ReceiptSpec.js
+++ jasmine/jasmine-standalone-3.1.0/spec/ReceiptSpec.js
@@ -7,5 +7,14 @@ describe("Unit Test: ", function () {
     receipt = new Receipt();
   });
 
+  describe('Receipt', function(){
+    it('#initialize: When the Receipt object is initialized', function() {
+      expect(receipt._order).toEqual(" ");
+    });
 
+    it('#calculateReceipt: Calculates the price of an order', function() {
+      receipt._order = ['{Cafe Latte: 4.75}, {Flat White: 4.75}'];
+      expect(receipt.calculate()).toEqual(9.50);
+    });
+  });
 });
