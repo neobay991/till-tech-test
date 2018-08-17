8e678c62f7ff0539a0912d4577bbb37e6d9434c8
Jay Khan <jkhan992@gmail.com>
2018-08-17 14:23:08 +0100

    TESTS Add Order unit test

---
+++ jasmine/jasmine-standalone-3.1.0/spec/OrderSpec.js
@@ -0,0 +1,29 @@
+'use-strict';
+
+describe("Unit Test: ", function () {
+  var menu;
+  var order;
+  var calculateOrder;
+
+  beforeEach(function(){
+    menu = new Menu();
+    calculateOrder = new CalculateOrder();
+    order = new Order(menu, receipt, calculateOrder);
+  });
+  describe('Order', function(){
+    describe('#addItem', function(){
+      it('Adds an item to an Order', function() {
+        order.addItem('"Cafe Latte": 4.75');
+        expect(order._customerOrder).toEqual('"Cafe Latte": 4.75');
+      });
+    });
+
+    describe('#calculateReceipt', function(){
+      it('Prints out an ORder', function() {
+        order.addItem('"Cafe Latte": 4.75');
+        expect(order.viewOrder()).toEqual('"Cafe Latte": 4.75', );
+      });
+    });
+  });
+
+});
