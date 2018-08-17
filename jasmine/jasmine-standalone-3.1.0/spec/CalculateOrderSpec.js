e94b32ba0e85e385689082758456731af41acfea
Jay Khan <jkhan992@gmail.com>
2018-08-17 14:26:06 +0100

    OTHER Fix typo

--- jasmine/jasmine-standalone-3.1.0/spec/CalculateOrderSpec.js
+++ jasmine/jasmine-standalone-3.1.0/spec/CalculateOrderSpec.js
@@ -1,4 +1,4 @@
-'use-strict';
+'use strict';
 
 describe("Unit Test: ", function () {
   var menu;
--- jasmine/jasmine-standalone-3.1.0/spec/FeatureTestSpec.js
+++ jasmine/jasmine-standalone-3.1.0/spec/FeatureTestSpec.js
@@ -1,4 +1,4 @@
-'use-strict';
+'use strict';
 
 describe("Feature Test: ", function () {
   var menu;
