31b682b3970aca79c57987a9040424963ba549fc
Jay Khan <jkhan992@gmail.com>
2018-08-18 18:48:04 +0100

    FEATURE Fix format of menu output, updated Menu unit test to relfect updated format

--- jasmine/jasmine-standalone-3.1.0/spec/MenuSpec.js
+++ jasmine/jasmine-standalone-3.1.0/spec/MenuSpec.js
@@ -8,60 +8,11 @@ describe("Unit Test: ", function () {
   });
 
   describe('Menu', function(){
-    describe('#initialize', function(){
-      it('Stores a JSON file containing shop and menu information', function() {
-        expect(menu._jsonFile).toEqual([{
-          "shopName": "The Coffee Connection",
-          "address": "123 Lakeside Way",
-          "phone": "16503600708",
-          "prices": [
-            {
-              "Cafe Latte": 4.75,
-              "Flat White": 4.75,
-              "Cappucino": 3.85,
-              "Single Espresso": 2.05,
-              "Double Espresso": 3.75,
-              "Americano": 3.75,
-              "Cortado": 4.55,
-              "Tea": 3.65,
-              "Choc Mudcake": 6.40,
-              "Choc Mousse": 8.20,
-              "Affogato": 14.80,
-              "Tiramisu": 11.40,
-              "Blueberry Muffin": 4.05,
-              "Chocolate Chip Muffin": 4.05,
-              "Muffin Of The Day": 4.55
-            }
-          ]
-        }]);
-      });
-    });
-
-    describe('#printMenu', function(){
+    describe('#getMenu', function(){
       it('Prints out a menu', function() {
-        expect(menu.printMenu()).toEqual([{
-          "prices": [
-            {
-              "Cafe Latte": 4.75,
-              "Flat White": 4.75,
-              "Cappucino": 3.85,
-              "Single Espresso": 2.05,
-              "Double Espresso": 3.75,
-              "Americano": 3.75,
-              "Cortado": 4.55,
-              "Tea": 3.65,
-              "Choc Mudcake": 6.40,
-              "Choc Mousse": 8.20,
-              "Affogato": 14.80,
-              "Tiramisu": 11.40,
-              "Blueberry Muffin": 4.05,
-              "Chocolate Chip Muffin": 4.05,
-              "Muffin Of The Day": 4.55
-            }
-          ]
-        }]);
+        expect(menu.getMenu()).toBe('The Coffee Connection\n \n123 Lakeside Way\nPhone: +1 (650) 360-0708\n \n{"Cafe Latte":4.75,"Flat White":4.75,"Cappucino":3.85,"Single Espresso":2.05,"Double Espresso":3.75,"Americano":3.75,"Cortado":4.55,"Tea":3.65,"Choc Mudcake":6.4,"Choc Mousse":8.2,"Affogato":14.8,"Tiramisu":11.4,"Blueberry Muffin":4.05,"Chocolate Chip Muffin":4.05,"Muffin Of The Day":4.55}');
       });
-    });
+    });    
   });
 
 });
--- jasmine/jasmine-standalone-3.1.0/src/Menu.js
+++ jasmine/jasmine-standalone-3.1.0/src/Menu.js
@@ -1,1 +1,39 @@
-'use-strict';
+'use strict';
+(function(exports){
+
+var priceList;
+var shopDetails;
+
+function Menu() {}
+
+Menu.prototype.getMenu = function() {
+  this.output1 = [];
+  $.get('src/hipstercoffee.json',function(data){
+    priceList = data[0].prices[0];
+    shopDetails = (data[0].shopName + '\n \n' + data[0].address + '\n' + "Phone: " + "+" + data[0].phone[0] + " " + "(" + data[0].phone[1] + data[0].phone[2] + data[0].phone[3] + ") " + data[0].phone[4] + data[0].phone[5] + data[0].phone[6] + "-" + data[0].phone[7] + data[0].phone[8] + data[0].phone[9] + data[0].phone[10])
+      console.log(shopDetails + "\n \n" + JSON.stringify(priceList));
+        // console.log(shopDetails);
+  });
+    return shopDetails + '\n \n' + JSON.stringify(priceList);
+};
+
+
+// Menu.prototype.storeMenu = function() {
+//   this.jsonFile = this.temp1.responseText;
+//
+// }
+
+// Menu.prototype.getPeople = function() {
+//   console.log(8);
+//   $.get("https://async-workshops-api.herokuapp.com/people", function(response) {
+//     this._jsonFile
+//     response.forEach(function(person) {
+//       console.log(person.name)
+//     });
+//     console.log(11);
+//   });
+//   console.log(9);
+// }
+Menu.prototype.getMenu();
+exports.Menu = Menu;
+})(this);
