872face4f0f18ea82b71910f822f001b44b10bbc
Jay Khan <jkhan992@gmail.com>
2018-08-19 00:43:00 +0100

    TESTS Update CalculateOrder unit test, wrap each method with describe. Update unit test for calculateOrderWithTax

--- jasmine/jasmine-standalone-3.1.0/spec/CalculateOrderSpec.js
+++ jasmine/jasmine-standalone-3.1.0/spec/CalculateOrderSpec.js
@@ -8,26 +8,34 @@ describe("Unit Test: ", function () {
   });
 
   describe('CalculateOrder', function(){
-
     describe('#calculate', function(){
       xit('Calculates an Order', function() {
         calculateOrder.calculate(8.60);
         calculateOrder.calculate(5.55);
         expect(calculateOrder.calculate(5.00)).toEqual(19.15);
       });
+    });
 
-      it('Calculates 8.64% tax on an Order', function() {
+    describe('#calculate', function(){
+      xit('Calculates 8.64% tax on an Order', function() {
         expect(calculateOrder.calculateTax(19.15)).toEqual(1.65);
-      });
-
-      xit('Calculates an Order with 8.64% tax', function() {
-        calculateOrder.calculate(8.60);
-        calculateOrder.calculate(5.55);
-        calculateOrder.calculate(5.00);
-        expect(calculateOrder.calculateOrderWithTax()).toEqual(20.80);
       });
     });
 
+    describe('#calculate', function(){
+      it('Calculates an Order with 8.64% tax', function() {
+        calculateOrder.calculate(4.75);
+        calculateOrder.calculate(4.75);
+        calculateOrder.calculate(4.75);
+        calculateOrder.calculate(3.85);
+        calculateOrder.calculate(6.40);
+        calculateOrder.calculate(6.40);
+        calculateOrder.calculate(8.20);
+        calculateOrder.calculate(14.80);
+        calculateOrder.calculate(11.40);
+        expect(calculateOrder.calculateOrderWithTax()).toEqual(70.94);
+      });
+    });
   });
 
 });
