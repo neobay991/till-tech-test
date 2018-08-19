8465c7ea4d3d781a0992a6162e82f02ab02bbdc6
Jay Khan <jkhan992@gmail.com>
2018-08-19 12:30:19 +0100

    OTHER REFACTOR Rename objects in ClaculateOrder class to indicate they are private

--- jasmine/jasmine-standalone-3.1.0/src/CalculateOrder.js
+++ jasmine/jasmine-standalone-3.1.0/src/CalculateOrder.js
@@ -2,7 +2,7 @@
 
 function CalculateOrder() {
   this._orderTotal = 0.00;
-  this.orderTax = 0.00;
+  this._orderTax = 0.00;
   this.orderTotalWithTax = 0.00;
 }
 
@@ -14,15 +14,15 @@ CalculateOrder.prototype.calculate = function(customerOrder) {
 
 CalculateOrder.prototype.calculateTax = function(orderTotal) {
   // Use Number to convert the string into a Number after calling toFixed();
-  this.orderTax = Number(parseFloat(orderTotal * 0.0864).toFixed(2));
-  return this.orderTax;
+  this._orderTax = Number(parseFloat(orderTotal * 0.0864).toFixed(2));
+  return this._orderTax;
 }
 
 CalculateOrder.prototype.calculateOrderWithTax = function() {
-  this.orderTotalWithTax = Number(parseFloat(this._orderTotal + this.orderTax).toFixed(2));
+  this.orderTotalWithTax = Number(parseFloat(this._orderTotal + this._orderTax).toFixed(2));
   return this.orderTotalWithTax;
 }
 
 CalculateOrder.prototype.returnTaxAmount = function() {
-  return this.orderTax;
+  return this._orderTax;
 }
