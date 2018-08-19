c2b9f95dbba78f8decee9222163a195775b6bf4a
Jay Khan <jkhan992@gmail.com>
2018-08-19 23:54:29 +0100

    OTHER Dry out Payment function into smaller functions

--- jasmine/jasmine-standalone-3.1.0/src/Payment.js
+++ jasmine/jasmine-standalone-3.1.0/src/Payment.js
@@ -4,7 +4,6 @@ function Payment(){
   this._customerPayment = 0;
   this._customerPaymentProcessed = false;
   this._customerPaymentChange = 0.00;
-  this._billDifference = 0.00;
 }
 
 Payment.prototype.processPayment = function(bill, payment) {
@@ -12,21 +11,33 @@ Payment.prototype.processPayment = function(bill, payment) {
   var payment_error = "Error: The order total is more than your payment please check your payment amount"
 
     if (payment > bill) {
-      this._customerPayment = payment;
-      this._customerPaymentChange = (payment - bill);
-      this._customerPaymentChange = Number(parseFloat(this._customerPaymentChange).toFixed(2));
+      this.savePayment(payment);
+      this.change(payment, bill);
       this._customerPaymentProcessed = true;
 
     } else if (payment < bill) {
         this._customerPaymentProcessed = false;
     } else {
-        this._customerPayment = payment;
+        this.savePayment(payment);
         this._customerPaymentProcessed = true;
     };
 
-    if (this._customerPaymentProcessed === true) {
-      return "Bill: " + bill + " " + " Payment amount: " + this._customerPayment + " Change: " + this._customerPaymentChange + " " + payment_confirmation;
-    } else {
-      return payment_error;
-    }
+    return this._customerPaymentProcessed;
+    //
+    // if (this._customerPaymentProcessed === true) {
+    //   return "Bill: " + bill + " " + " Payment amount: " + this._customerPayment + " Change: " + this._customerPaymentChange + " " + payment_confirmation;
+    // } else {
+    //   return payment_error;
+    // }
+}
+
+Payment.prototype.savePayment = function(payment) {
+  this._customerPayment = payment;
+  return this._customerPayment;
+}
+
+Payment.prototype.change = function(payment, bill) {
+  // Use Number to convert the string into a Number after calling toFixed();
+  this._customerPaymentChange = Number(parseFloat(payment - bill).toFixed(2));
+  return this._customerPaymentChange;
 }
