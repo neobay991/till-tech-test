f80b0832fb63b923dbd6d33e49013a924508a512
Jay Khan <jkhan992@gmail.com>
2018-08-19 23:59:24 +0100

    TESTS Add Additonal unit tests for Payment spec after DRY out Payment function into smaller functions

--- jasmine/jasmine-standalone-3.1.0/spec/PaymentSpec.js
+++ jasmine/jasmine-standalone-3.1.0/spec/PaymentSpec.js
@@ -13,6 +13,10 @@ describe("Unit Test: ", function () {
         expect(payment.processPayment(10, 15)).toEqual(true);
       });
 
+      it('Processes a payment and returns true if the payment amount is equal to the balance', function() {
+        expect(payment.processPayment(10, 10)).toEqual(true);
+      });
+
       it('Processes a payment and returns false if the payment amount is less than the balance', function() {
         expect(payment.processPayment(10, 5)).toEqual(false);
       });
@@ -21,6 +25,10 @@ describe("Unit Test: ", function () {
     describe('#change', function(){
       it('returns the change from a payment', function() {
         expect(payment.change(15, 10)).toEqual(5);
+      });
+
+      it('returns the no change if the payment is equal to the balance', function() {
+        expect(payment.change(15, 15)).toEqual(0);
       });
     });
   });
