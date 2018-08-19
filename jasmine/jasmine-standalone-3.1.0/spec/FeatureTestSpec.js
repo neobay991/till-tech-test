617d1ce6a0a247bc889070733ee0629bd8ed1d37
Jay Khan <jkhan992@gmail.com>
2018-08-19 23:30:47 +0100

    TESTS Fix failing tests so they include payment amount and change

--- jasmine/jasmine-standalone-3.1.0/spec/FeatureTestSpec.js
+++ jasmine/jasmine-standalone-3.1.0/spec/FeatureTestSpec.js
@@ -61,17 +61,18 @@ describe("Feature Test: ", function () {
       order.addTable(1, 2, "Jane, John");
       order.addItem(1, '"Cafe Latte": 4.75');
       order.addItem(2, '"Chocolate Chip Muffin": 4.05');
-      var menuHeader = 'The Coffee Connection\n\n123 Lakeside Way\nPhone: +1 (650) 360-0708'
-      var customerTableNumber = 'Table: 1 / '
-      var customerTableCustomers = '[2]';
-      var customerTableCustomersNames = 'Jane, John'
-      var customerOrder = '1 x "Cafe Latte": 4.75\n2 x "Chocolate Chip Muffin": 4.05\n'
-      var customerOrderTax = 1.11;
-      var customerOrderWithTax = 13.96;
-      var menuFooter = 'Thank you!'
-      var receiptOutput = menuHeader + "\n\n" + customerTableNumber + customerTableCustomers + "\n" + customerTableCustomersNames + "\n" + customerOrder + "\n" + "Tax " + "$" + customerOrderTax + "\n" + "Total: " + "$" +  customerOrderWithTax + "\n" + menuFooter;
-      receipt.getReceipt(receiptOutput);
-      expect(order.submitOrder()).toEqual('The Coffee Connection\n\n123 Lakeside Way\nPhone: +1 (650) 360-0708\n\nTable: 1 / [2]\nJane, John\n1 x "Cafe Latte": 4.75\n2 x "Chocolate Chip Muffin": 4.05\n\nTax $1.11\nTotal: $13.96\nThank you!');
+      order.makePayment(13.96, 15.00);
+      // var menuHeader = 'The Coffee Connection\n\n123 Lakeside Way\nPhone: +1 (650) 360-0708'
+      // var customerTableNumber = 'Table: 1 / '
+      // var customerTableCustomers = '[2]';
+      // var customerTableCustomersNames = 'Jane, John'
+      // var customerOrder = '1 x "Cafe Latte": 4.75\n2 x "Chocolate Chip Muffin": 4.05\n'
+      // var customerOrderTax = 1.11;
+      // var customerOrderWithTax = 13.96;
+      // var menuFooter = 'Thank you!'
+      // var receiptOutput = menuHeader + "\n\n" + customerTableNumber + customerTableCustomers + "\n" + customerTableCustomersNames + "\n" + customerOrder + "\n" + "Tax " + "$" + customerOrderTax + "\n" + "Total: " + "$" +  customerOrderWithTax + "\n" + menuFooter;
+      // receipt.getReceipt(receiptOutput);
+      expect(order.submitOrder()).toEqual('The Coffee Connection\n\n123 Lakeside Way\nPhone: +1 (650) 360-0708\n\nTable: 1 / [2]\nJane, John\n1 x "Cafe Latte": 4.75\n2 x "Chocolate Chip Muffin": 4.05\n\nTax $1.11\nTotal: $13.96\nCash: 15\nChange: $1.04\nThank you!');
     });
   });
 
--- jasmine/jasmine-standalone-3.1.0/spec/ReceiptSpec.js
+++ jasmine/jasmine-standalone-3.1.0/spec/ReceiptSpec.js
@@ -18,8 +18,10 @@ describe("Unit Test: ", function () {
         var customerOrder = '1 x "Cafe Latte": 4.75\n2 x "Chocolate Chip Muffin": 4.05\n'
         var customerOrderTax = 1.11;
         var customerOrderWithTax = 13.96;
+        var customerPayment = 15.00;
+        var customerPaymentChange = 1.04;
         var menuFooter = 'Thank you!'
-        expect(receipt.getReceipt(menuHeader, customerTableNumber, customerTableCustomers, customerTableCustomersNames, customerOrder, customerOrderTax, customerOrderWithTax, menuFooter)).toEqual('The Coffee Connection\n\n123 Lakeside Way\nPhone: +1 (650) 360-0708\n\nTable: 1 / [2]\nJane, John\n1 x "Cafe Latte": 4.75\n2 x "Chocolate Chip Muffin": 4.05\n\nTax $1.11\nTotal: $13.96\nThank you!');
+        expect(receipt.getReceipt(menuHeader, customerTableNumber, customerTableCustomers, customerTableCustomersNames, customerOrder, customerOrderTax, customerOrderWithTax, customerPayment, customerPaymentChange, menuFooter)).toEqual('The Coffee Connection\n\n123 Lakeside Way\nPhone: +1 (650) 360-0708\n\nTable: 1 / [2]\nJane, John\n1 x "Cafe Latte": 4.75\n2 x "Chocolate Chip Muffin": 4.05\n\nTax $1.11\nTotal: $13.96\nCash: 15\nChange: $1.04\nThank you!');
       });
     });
 
