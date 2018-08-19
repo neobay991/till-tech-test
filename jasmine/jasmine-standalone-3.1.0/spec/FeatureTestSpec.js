8143ccda32741c500490948c91d9bead1f0fdaa3
Jay Khan <jkhan992@gmail.com>
2018-08-19 01:45:59 +0100

    TESTS Update tests to include user per order

--- jasmine/jasmine-standalone-3.1.0/spec/FeatureTestSpec.js
+++ jasmine/jasmine-standalone-3.1.0/spec/FeatureTestSpec.js
@@ -20,10 +20,18 @@ describe("Feature Test: ", function () {
   });
 
   describe('View order', function(){
-    it('A User can add quatity and view their order', function() {
+    it('A User can add users, quatity and view their order', function() {
+      order.addItem('"Jane"', 1, '"Cafe Latte": 4.75');
+      order.addItem('"John"', 2, '"Chocolate Chip Muffin": 4.05');
+      expect(order.viewOrder()).toEqual('Jane: 1 x "Cafe Latte": 4.75,John: 2 x "Chocolate Chip Muffin": 4.05\nTax: 1.11\nBalance: 13.96');
+    });
+  });
+
+  describe('View receipt', function(){
+    xit('A User can add view a receipt of their order', function() {
       order.addItem(1, '"Cafe Latte": 4.75');
       order.addItem(2, '"Chocolate Chip Muffin": 4.05');
-      expect(order.viewOrder()).toEqual('1 x "Cafe Latte": 4.75,2 x "Chocolate Chip Muffin": 4.05\nTax: 1.11\nBalance: 13.96');
+      expect(order.viewReceipt()).toEqual('The Coffee Connection\n \n123 Lakeside Way\nPhone: +1 (650) 360-0708\n \n{"Cafe Latte":4.75,"Flat White":4.75,"Cappucino":3.85,"Single Espresso":2.05,"Double Espresso":3.75,"Americano":3.75,"Cortado":4.55,"Tea":3.65,"Choc Mudcake":6.4,"Choc Mousse":8.2,"Affogato":14.8,"Tiramisu":11.4,"Blueberry Muffin":4.05,"Chocolate Chip Muffin":4.05,"Muffin Of The Day":4.55}');
     });
   });
 
